131e6b652906378b55eb5a74e0063d797c23178d
unknown <raylanes315@gmail.com>
2023-04-02 16:26:19 -0300

    Objetos

---
+++ Teste_altomovel.java
@@ -0,0 +1,12 @@
+package POO;
+
+public class Teste_altomovel {
+
+	public static void main(String[] args) {
+		
+		
+		Altomovel leia = new Altomovel("Ray","Uno","XX45",2023);
+		leia.visualizar();
+	}
+
+}
